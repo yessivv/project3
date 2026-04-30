3514bfa54ea1015cf5ab086a2a9f34955d3acc1d
yessivv <vegay337@newschool.edu>
2026-04-29 23:39:07 -0400

    iwanttokilleverything

--- script.js
+++ script.js
@@ -1,12 +1,42 @@
-const { google } = require('googleapis');
-const drive = google.drive({ version: 'v3', auth });
+const port = process.env.PORT || 3000;
 
-async function uploadFile(fileName, content) {
-  await drive.files.create({
-    requestBody: { name: fileName, mimeType: 'application/json' },
-    media: { mimeType: 'application/json', body: JSON.stringify(content) },
+const CLIENT_ID = 'YOUR_CLIENT_ID';
+const API_KEY = 'AIzaSyBkYpIxrJh1yFPe4_GrjbT8UKuDFPOIqwY';
+const SCOPES = 'https://www.googleapis.com/auth/drive.readonly';
+
+// Initialize the API client
+async function initGoogleApi() {
+  await gapi.load('client', async () => {
+    await gapi.client.init({
+      apiKey: API_KEY,
+      discoveryDocs: ['https://www.googleapis.com/discovery/v1/apis/drive/v3/rest'],
+    });
   });
 }
+
+// Sign in the user
+function signIn() {
+  const tokenClient = google.accounts.oauth2.initTokenClient({
+    client_id: CLIENT_ID,
+    scope: SCOPES,
+    callback: (response) => {
+      if (response.access_token) {
+        listFiles(); // Proceed after auth
+      }
+    },
+  });
+  tokenClient.requestAccessToken();
+}
+
+// List files from Drive
+async function listFiles() {
+  const response = await gapi.client.drive.files.list({
+    pageSize: 10,
+    fields: 'files(id, name, mimeType)',
+  });
+  console.log(response.result.files);
+}
+
 
 const stars = document.querySelectorAll('#star');
 
